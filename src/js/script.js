97fcc5d725406a315883123a8c1ea72bda498ffe
Oleg <77981576.ok@gmail.com>
2025-07-07 17:42:47 +0300

    lesson_86_works-block

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,36 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import '/src/sass/style.scss';
+
+try {
+  new Swiper('.works__slider', {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.icon-right',
+      prevEl: '.icon-left',
+    },
+    loop: true,
+    breakpoints: {
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1920: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      },
+    },
+    modules: [Navigation, Pagination],
+  });
+} catch (e) {}

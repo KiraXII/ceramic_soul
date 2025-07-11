c0b390614d5b542c156e1828bbc38401704a0214
Oleg <77981576.ok@gmail.com>
2025-07-11 12:38:27 +0300

    commit__lesson__92-catalog__page

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,39 @@ try {
     modules: [Navigation, Pagination],
   });
 } catch (e) {}
+
+try {
+  // const tabs = document.querySelector('.catalog__tab');
+  // const contents = document.querySelector('.catalog__content-item');
+  // tabs.forEach((tab, index) => {
+  //   tab.addEventListener('click', () => {
+  //     //удаляем активный класс у всех табов и контента
+  //     tabs.forEach((t) => t.classList.remove('catalog__tab_active'));
+  //     contents.forEach((c) => (c.style.display = 'none'));
+  //     // добавляем активный класс к нажатому табу и показываем соответствующий контент
+  //     tab.classList.add('.catalog__tab_active');
+  //     contents[index].style.display = 'block';
+  //   });
+  // });
+  // //показываем первый контент при загрузке
+  // contents.forEach((c, i) => (c.style.display = i === 0 ? 'block' : 'none'));
+} catch (e) {}
+
+const tabs = document.querySelectorAll('.catalog__tab');
+const contents = document.querySelectorAll('.catalog__content__wrapper');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    //удаляем активный класс у всех табов и контента
+    tabs.forEach((t) => t.classList.remove('catalog__tab_active'));
+    contents.forEach((c) => (c.style.display = 'none'));
+
+    // добавляем активный класс к нажатому табу и показываем соответствующий контент
+    tab.classList.add('catalog__tab_active');
+    contents[index].style.display = 'block';
+  });
+});
+
+//показываем первый контент при загрузке
+
+contents.forEach((c, i) => (c.style.display = i === 0 ? 'block' : 'none'));

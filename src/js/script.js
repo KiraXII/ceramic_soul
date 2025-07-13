4b8c994e1718668be0e47c10f22a1a4f39a82506
Oleg <77981576.ok@gmail.com>
2025-07-13 07:49:24 +0300

    commit__validate__form-lesson_96

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+import JustValidate from 'just-validate';
 
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
@@ -68,4 +69,104 @@ try {
   //показываем первый контент при загрузке
 
   contents.forEach((c, i) => (c.style.display = i === 0 ? 'block' : 'none'));
+} catch (e) {}
+
+try {
+  const validator = new JustValidate('.git__form', {
+    errorLabelStyle: { color: '#f14668' },
+  });
+
+  validator
+    .addField('#name', [
+      {
+        rule: 'required',
+      },
+      {
+        rule: 'minLength',
+        value: 2,
+      },
+    ])
+    .addField('#email', [
+      {
+        rule: 'required',
+      },
+      {
+        rule: 'email',
+      },
+    ])
+    .addField(
+      '#question',
+      [
+        {
+          rule: 'required',
+        },
+        {
+          rule: 'minLength',
+          value: 5,
+        },
+      ],
+      {
+        errorsContainer: document
+          .querySelector('#question')
+          .parentElement.querySelector('.error-message'),
+      }
+    )
+    .addField(
+      '#checkbox',
+      [
+        {
+          rule: 'required',
+        },
+      ],
+      {
+        errorsContainer: document
+          .querySelector('#question')
+          .parentElement.parentElement.querySelector('.checkbox-error-message'),
+      }
+    );
+} catch (e) {}
+
+try {
+  const footerValidator = new JustValidate('.footer__input__wrapper', {
+    errorLabelStyle: { color: '$ui-error' },
+  });
+
+  footerValidator
+
+    .addField(
+      '#footer-email',
+      [
+        {
+          rule: 'required',
+        },
+        {
+          rule: 'email',
+        },
+      ],
+
+      {
+        errorsContainer: document
+          .querySelector('#footer-email')
+          .parentElement.parentElement.querySelector(
+            '.footer-email-error-message'
+          ),
+      }
+    )
+
+    .addField(
+      '#footer-checkbox',
+      [
+        {
+          rule: 'required',
+        },
+      ],
+
+      {
+        errorsContainer: document
+          .querySelector('#footer-checkbox')
+          .parentElement.parentElement.querySelector(
+            '.footer-checkbox-error-message'
+          ),
+      }
+    );
 } catch (e) {}

fde58f6fcc8274d6926465db6083ff506da270a4
Oleg <77981576.ok@gmail.com>
2025-07-14 21:21:16 +0300

    commit_lesson_98_form-fetch

--- src/js/script.js
+++ src/js/script.js
@@ -123,7 +123,21 @@ try {
           .querySelector('#question')
           .parentElement.parentElement.querySelector('.checkbox-error-message'),
       }
-    );
+    )
+    .onSuccess((event) => {
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+
+      fetch('https://httpbin.org/post', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('Success', data);
+          form.reset();
+        });
+    });
 } catch (e) {}
 
 try {
